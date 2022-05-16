eefa3739aa9124127d783172ad12cc45ab33f005
Saurabh Dabas <developerdabas@gmail.com>
2022-05-16 13:14:40 +0530

    Function report back how many instances of each string were found in the allItems array of strings

---
+++ Katas/countOnly.js
@@ -0,0 +1,28 @@
+// The function should report back how many instances of each string were found in the allItems array of strings.
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const allItems = ["a","a","b","c","d","e","a"];
+const itemsToCount = {
+  a:true,
+  d:true,
+  b:false,
+  f:true
+}
+
+const countOnly = function(allItems, itemsToCount) {
+  let finalObj = {};
+  for(let item of allItems) {
+    if(itemsToCount[item] === true){
+      if(!finalObj[item]){
+        finalObj[item] = 1;
+      } else {
+        finalObj[item] += 1;
+      }
+    }
+  }
+  return finalObj;
+}
+console.log(countOnly(allItems,itemsToCount));
