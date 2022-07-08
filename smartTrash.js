7f0aa959aa4d8b137c5b4b3c088951052a14cdfc
Saurabh Dabas <developerdabas@gmail.com>
2022-07-08 22:55:26 +0530

    smart trash fucntion

--- smartTrash.js
+++ smartTrash.js
@@ -13,5 +13,6 @@ const smartGarbage = (trash,bins) => {
   return bins;
 }
 console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }))
+console.log(smartGarbage('recycling', { waste: 8, recycling: 2, compost: 5 }))
 console.log(smartGarbage('waste', { waste: 4, recycling: 2, compost: 5 }))
 console.log(smartGarbage('compost', { waste: 4, recycling: 2, compost: 5 }))
