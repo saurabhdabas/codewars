8257b55010d76a48dc01f1fa210d056707e01cfc
Saurabh Dabas <developerdabas@gmail.com>
2022-07-09 23:45:27 +0530

    transpose of 2d matrix

--- arrayTranspose.js
+++ arrayTranspose.js
@@ -14,8 +14,8 @@ for(let i = 0 ; i < arr[0].length ;  i ++ ){  // we want i to loop till the leng
 console.log(transposedArray);
 return transposedArray ;
 }
-
+const arr = [ [1,2,3],[4,5,6],[7,8,9]];
 transpose(arr);
 
-// const arr = [ [1,2,3],[4,5,6],[7,8,9]];
+console.log(transpose(arr));
 // const arr = [ [1,2],[4,5],[7,8]];
